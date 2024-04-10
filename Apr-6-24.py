b964d43dde43c75f070b72123e65e87c5201ef52
ram_tanniru <ramtanniru2@gmail.com>
2024-04-10 20:40:24 +0530

    6-4-24

---
+++ Apr-6-24.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countWays(self, n):
+        return 1+n//2
+        
+        # Sequence
+        # 1->1
+        # 2->2
+        # 3->2
+        # 4->3
+        # 5->3
+        # 6->4
+        # 7->4
