b763c32101d9d14d4e6e452fbcd3c15bc896f122
ram_tanniru <ramtanniru2@gmail.com>
2024-05-12 17:59:09 +0530

    12-5-24

---
+++ May-12-24.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minSteps(self, d):
+        ans = 0
+        temp = 0
+        i = 1
+        while temp<d:
+            temp += i
+            i += 1
+            ans += 1
+        if temp==d:
+            return ans
+        if temp>d:
+            if (d-temp)%2==0:
+                return ans
+            else:
+                if (ans)%2==0:
+                    return ans+1
+                else:
+                    return ans+2
+        return ans
