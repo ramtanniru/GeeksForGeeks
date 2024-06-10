793689a3d38bd0b9ba6f445dd730f42384288792
ram_tanniru <ramtanniru2@gmail.com>
2024-06-10 11:25:40 +0530

    10-6-24

---
+++ Jun-10-24.py
@@ -0,0 +1,11 @@
+class Solution:
+	def matchPairs(self, n, nuts, bolts):
+		x = set(nuts)
+		order = ['!','#','$','%','&','*','?','@','^']
+		idx = 0
+		for i in order:
+		    if i in x:
+		        nuts[idx] = i
+		        bolts[idx] = i
+		        idx += 1
+		return
