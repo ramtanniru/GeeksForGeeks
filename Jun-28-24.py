45e3e35fd146948e05bad16acc9f16cc1900607d
ram_tanniru <ramtanniru2@gmail.com>
2024-06-28 21:47:07 +0530

    The Palindrome Pattern

---
+++ Jun-28-24.py
@@ -0,0 +1,18 @@
+class Solution:
+    def pattern (self, arr):
+        # rows
+        for i in range(len(arr)):
+            if arr[i] == arr[i][::-1]:
+                return str(i)+" R"
+        # columns
+        for i in range(len(arr[0])):
+            x,y = 0,len(arr)-1
+            while x<y:
+                if arr[x][i] == arr[y][i]:
+                    x += 1
+                    y -= 1
+                else:
+                    break
+            if x>=y:
+                return str(i)+" C" 
+        return -1 
