66d4c65445497d18cc9caf379ee285c80e8122ad
ram_tanniru <ramtanniru2@gmail.com>
2024-07-10 06:09:51 +0530

    Largest square formed in a matrix

---
+++ Jul-10-24.py
@@ -0,0 +1,11 @@
+dp = [[0]*len(mat[0]) for i in range(len(mat))]
+        maxArea = 0
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                if mat[i][j]==1:
+                    if i==0 or j==0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1])+1
+                    maxArea = max(maxArea,dp[i][j])
+        return maxArea 
