980ddbd74f454b357f1aa3e2139da14a69ae8506
ram_tanniru <ramtanniru2@gmail.com>
2024-08-07 11:20:33 +0530

    K-th element of two arrays

---
+++ Aug-7-24.py
@@ -0,0 +1,20 @@
+class Solution:
+    def kthElement(self, k, arr1, arr2):
+        def merge(a,b):
+            res = []
+            i,j = 0,0
+            while i<len(a) and j<len(b):
+                if a[i]<b[j]:
+                    res.append(a[i])
+                    i += 1
+                else:
+                    res.append(b[j])
+                    j += 1
+            while i<len(a):
+                res.append(a[i])
+                i += 1
+            while j<len(b):
+                res.append(b[j])
+                j += 1
+            return res
+        return merge(arr1,arr2)[k-1] 
