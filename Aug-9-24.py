814a4d2b68ab81bdfbf2e4caa9e578d91e759f64
ram_tanniru <ramtanniru2@gmail.com>
2024-08-09 10:35:46 +0530

    Maximise array value after rearrangement

---
+++ Aug-9-24.py
@@ -0,0 +1,8 @@
+class Solution:
+    def Maximize(self, a): 
+        mod = 10**9 + 7
+        a.sort()
+        s = 0
+        for i,x in enumerate(a):
+            s += i*x % mod
+        return s % mod 
