c3e6a98361bcaac997d6f4f41c6eecba557c583c
ram_tanniru <ramtanniru2@gmail.com>
2024-08-10 10:42:37 +0530

    Rotate a linked list

---
+++ Aug-10-24.py
@@ -0,0 +1,25 @@
+class Solution:
+    def rotate(self, head, k):
+        def findLength(head):
+            curr = head
+            res = 0
+            while curr:
+                res += 1
+                curr = curr.next
+            return res 
+        k = k%findLength(head)
+        if k==0:
+            return head 
+        temp = head
+        curr = head
+        cnt = 1
+        while cnt<k:
+            cnt += 1
+            curr = curr.next
+        temp = curr.next
+        curr.next = None
+        curr = temp
+        while curr.next:
+            curr = curr.next
+        curr.next = head
+        return temp 
