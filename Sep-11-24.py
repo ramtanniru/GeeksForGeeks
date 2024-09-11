1380ce3985f285fb36a67d4aaa3104689e0f18de
ram_tanniru <ramtanniru2@gmail.com>
2024-09-11 09:51:15 +0530

    Minimum cost of ropes

---
+++ Sep-11-24.py
@@ -0,0 +1,11 @@
+import heapq
+class Solution:
+   def minCost(self, arr):
+        heapq.heapify(arr)
+        cost = 0
+        while len(arr)>1:
+            a = heapq.heappop(arr)
+            b = heapq.heappop(arr)
+            cost += a+b
+            heapq.heappush(arr,a+b)
+        return cost 
