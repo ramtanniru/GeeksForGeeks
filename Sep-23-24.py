5b311f457d9dab1216c30be20fb59308984ffdee
ram_tanniru <ramtanniru2@gmail.com>
2024-09-23 23:00:17 +0530

    Missing and repeating

---
+++ Sep-23-24.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findTwoElement( self,arr): 
+        s = set(arr)
+        n = len(arr)
+        arrSum = sum(arr)
+        setSum = sum(list(s))
+        actualSum = (n*(n+1))//2
+        return [arrSum-setSum,actualSum-setSum] 
